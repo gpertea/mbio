76822b50ad4c4d953e83253f07bc12b9065ae2ee
corina <cantones@jhu.edu>
2017-08-07 21:19:31 -0400

    As a security measure Javascript cannot create files on client computers, hence the trick with form submissions. JQuery, while not necessary, is nevertheless convenient. Content Type is better to be something else than 'text/html'. Also, ideally, file size should be provided in the headers as well. N srj zbzragf gb yrnea, n yvsrgvzr gb znfgre.

--- cgi/single_sample_dnld.pl
+++ cgi/single_sample_dnld.pl
@@ -5,24 +5,6 @@ use CGI;
 
 my $q = CGI->new;
 
-# Read the one param and split it in the two elements (path and file)
-
-=begin
-
-print $q->header(
-	-type => 'application/octet-stream',
-	-attachment => 'my_awesome_file'
-	);
-
-
-print("resultate loklenonn");
-
-=end
-
-=cut
-
-
-# Binary files require something like this:
 
 my $buf_size = 4096;
 my $file_root = '/lion/scratch0/pathology/www-out/032217/7MMcDNA032217_S11_L001_R1_001.trim.fq.gz'; 
